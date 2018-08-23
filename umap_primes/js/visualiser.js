0ee6cc239320543fd42e4c530b32f4273f6b6921
John Williamson <johnhw@gmail.com>
2018-08-23 13:09:21 +0100

    Added explanation of the 2D plot

--- umap_primes/js/visualiser.js
+++ umap_primes/js/visualiser.js
@@ -1,9 +1,10 @@
  // for plasma colouring
 
  var url = new URL(window.location);
- var c = url.searchParams.get("npy");
- npy_fname = c || '1e6_pts_3d_int16.npy'
- 
+
+ var npy_fname = url.searchParams.get("npy") || '1e6_pts_3d_int16.npy'
+ var color_mode = url.searchParams.get("color") || 'factors'
+
 
  var plasma_cmap = interpolateArray(plasma);
  var n = 1000000;
@@ -18,39 +19,34 @@
 
      // check if we are filtering, simple colouring, or full colouring
      var test_result = _pred(5);
-     if(typeof(test_result)=="boolean")
-     {
-        for (i = 0; i < n; i++) {
-            if (_pred(i)) {
-                colours[i * 3] = col_data[i * 3] / 255.0;
-                colours[i * 3 + 1] = col_data[i * 3 + 1] / 255.0;
-                colours[i * 3 + 2] = col_data[i * 3 + 2] / 255.0;
-            } else {
-                colours[i * 3] = 0.1;
-                colours[i * 3 + 1] = 0.1;
-                colours[i * 3 + 2] = 0.1;
-            }
-        }
-    }
-    else if(typeof(test_result)=="number")
-    {
-       for (i = 0; i < n; i++) {
-           var rgb = plasma_cmap(_pred(i));           
-            colours[i * 3] = rgb.r;
-            colours[i * 3 + 1] = rgb.g;
-            colours[i * 3 + 2] = rgb.b;        
-       }
-   }
-   else if(test_result.length===3)
-   {
-        for (i = 0; i < n; i++) {
-            var rgb = _pred(i);           
-            colours[i * 3] = rgb[0];
-            colours[i * 3 + 1] = rgb[1];
-            colours[i * 3 + 2] = rgb[2];        
-        }
-   }
-   
+     if (typeof (test_result) == "boolean") {
+         for (i = 0; i < n; i++) {
+             if (_pred(i)) {
+                 colours[i * 3] = col_data[i * 3] / 255.0;
+                 colours[i * 3 + 1] = col_data[i * 3 + 1] / 255.0;
+                 colours[i * 3 + 2] = col_data[i * 3 + 2] / 255.0;
+             } else {
+                 colours[i * 3] = 0.1;
+                 colours[i * 3 + 1] = 0.1;
+                 colours[i * 3 + 2] = 0.1;
+             }
+         }
+     } else if (typeof (test_result) == "number") {
+         for (i = 0; i < n; i++) {
+             var rgb = plasma_cmap(_pred(i));
+             colours[i * 3] = rgb.r;
+             colours[i * 3 + 1] = rgb.g;
+             colours[i * 3 + 2] = rgb.b;
+         }
+     } else if (test_result.length === 3) {
+         for (i = 0; i < n; i++) {
+             var rgb = _pred(i);
+             colours[i * 3] = rgb[0];
+             colours[i * 3 + 1] = rgb[1];
+             colours[i * 3 + 2] = rgb[2];
+         }
+     }
+
      pt_buffer.attributes.color.needsUpdate = true;
  }
  // number theory functions
@@ -71,8 +67,8 @@
      function animate() {
          requestAnimationFrame(animate);
          // only update if focused
-         if(container===document.activeElement)
-            controls.update(1);
+         if (container === document.activeElement)
+             controls.update(1);
          renderer.render(scene, camera);
      }
 
@@ -100,19 +96,25 @@
 
      var pts = new Float32Array(n * 3);
      for (i = 0; i < n; i++) {
-         /* var rgb = plasma_cmap(i/n);
-                    
-         colours[i*3] = rgb.r;
-         colours[i*3+1] = rgb.g;
-         colours[i*3+2] = rgb.b;                    */
-
          pts[i * 3] = pt_array.data[i * 3] / 32768.0;
          pts[i * 3 + 1] = pt_array.data[i * 3 + 1] / 32768.0;
          pts[i * 3 + 2] = pt_array.data[i * 3 + 2] / 32768.0;
-         colours[i * 3] = col_array.data[i * 3] / 255.0;
-         colours[i * 3 + 1] = col_array.data[i * 3 + 1] / 255.0;
-         colours[i * 3 + 2] = col_array.data[i * 3 + 2] / 255.0;
      }
+     if (color_mode === "factors") {
+         for (i = 0; i < n; i++) {
+             colours[i * 3] = col_array.data[i * 3] / 255.0;
+             colours[i * 3 + 1] = col_array.data[i * 3 + 1] / 255.0;
+             colours[i * 3 + 2] = col_array.data[i * 3 + 2] / 255.0;
+         }
+     } else if (color_mode === "plasma") {
+         for (i = 0; i < n; i++) {
+             var rgb = plasma_cmap(i / n);
+             colours[i * 3] = rgb.r;
+             colours[i * 3 + 1] = rgb.g;
+             colours[i * 3 + 2] = rgb.b;
+         }
+     }
+
 
      pt_buffer.addAttribute('position', new THREE.BufferAttribute(pts, 3));
      pt_buffer.addAttribute('color', new THREE.BufferAttribute(colours, 3));
@@ -148,5 +150,5 @@
          onloaded(pt_array, col_array);
      });
  }
-console.log(npy_fname);
+ console.log(npy_fname);
  NumpyLoader.ajax(npy_fname, onloaded_stage_1);
